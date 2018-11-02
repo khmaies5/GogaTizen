4aa55c4fb6d8856652741d8406b406946725afdb
khmaies hassen <khmaieshassen@hotmail.com>
2018-11-02 16:56:20 +0100

    deleting useless files and adding posts methodes

--- js/index-mobile.js
+++ js/index-mobile.js
@@ -1,8 +1,46 @@
 window.onload = function() {
-
+    var posts;
 loadUserDetails();
 
+loadData().then(data=>{
 
+    for(var e in data){
+        console.log(data[e]);
+        posts += `<div class="row">
+        <div class="col s12">
+            <div class="entry">
+                <span class="category">Funny</span>
+                <img src="https://goga-api.herokuapp.com/api/attachments/images/download/${data[e].type}" alt="">
+                <h5><a href="#">${data[e].title}</a></h5>
+                <span class="author">By <a href="#">Mario Doe</a></span>
+                <p>Lorem ipsum dolor sit amet consectetur</p>
+            </div>
+        </div>
+    </div>`
+    }
+     $('#myList').append(posts);
+
+
+}).catch(err=>{
+
+    console.log(err)
+      $('#myList').append(`<div class="row">
+      <div class="col s12">
+          <div class="entry">
+              <span class="category">Funny</span>
+              <img src="img/loading.gif" alt="">
+              <h5><a href="#">error</a></h5>
+              <span class="author">By <a href="#">Admin</a></span>
+              <p>${err}</p>
+          </div>
+      </div>
+  </div>`);
+
+})
+
+
+
+      
 
 }
 
@@ -34,7 +72,21 @@ xhr.send();
 
 }
 
-function loadPosts(){
+
+const loadData = async () => {
+   const url = "https://goga-api.herokuapp.com/api/posts?filter=%7B%22order%22%3A%22datepublication%20DESC%22%7D";
+
+  
+      
+      let data =  (await fetch(url)).json();
+      
+    return data;
+  }
+
+
+
+
+ function loadPosts(){
     var xhr = new XMLHttpRequest();
 
     var url = "https://goga-api.herokuapp.com/api/posts?filter=%7B%22order%22%3A%22datepublication%20DESC%22%7D";
@@ -50,16 +102,48 @@ function loadPosts(){
            <div class="col s12">
                <div class="entry">
                    <span class="category">Funny</span>
-                   <img src="img/post2.jpg" alt="">
-                   <h5><a href="#">Crazy really shows you have to try</a></h5>
+                   <img src="https://goga-api.herokuapp.com/api/attachments/images/download/${json.type}" alt="">
+                   <h5><a href="#">${json.title}</a></h5>
                    <span class="author">By <a href="#">Mario Doe</a></span>
                    <p>Lorem ipsum dolor sit amet consectetur</p>
                </div>
            </div>
        </div>`
-
+       $('#myList').append(`<div class="row">
+       <div class="col s12">
+           <div class="entry">
+               <span class="category">Funny</span>
+               <img src="https://goga-api.herokuapp.com/api/attachments/images/download/${json.type}" alt="">
+               <h5><a href="#">${json.title}</a></h5>
+               <span class="author">By <a href="#">Mario Doe</a></span>
+               <p>Lorem ipsum dolor sit amet consectetur</p>
+           </div>
+       </div>
+   </div>`);
         }else {
             console.log("error");
+            var posts = `<div class="row">
+            <div class="col s12">
+                <div class="entry">
+                    <span class="category">Funny</span>
+                    <img src="/img/loading.gif" alt="">
+                    <h5><a href="#">Error</a></h5>
+                    <span class="author">By <a href="#">Admin</a></span>
+                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                </div>
+            </div>
+        </div>`
+            $('#myList').append(`<div class="row">
+            <div class="col s12">
+                <div class="entry">
+                    <span class="category">Funny</span>
+                    <img src="/img/loading.gif" alt="">
+                    <h5><a href="#">Error</a></h5>
+                    <span class="author">By <a href="#">Admin</a></span>
+                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                </div>
+            </div>
+        </div>`);
          //   loginResults()}
     } 
 

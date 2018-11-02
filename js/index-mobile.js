0f5555836f80b60552ce6b74dcbcabd1fbc45628
khmaies hassen <khmaieshassen@hotmail.com>
2018-11-02 14:29:09 +0100

    adding loader

--- js/index-mobile.js
+++ js/index-mobile.js
@@ -14,9 +14,7 @@ function loadUserDetails(){
     var url = "https://goga-api.herokuapp.com/api/users/"+userId;
     xhr.open("GET", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
-
-
-   
+ 
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var json = JSON.parse(xhr.responseText);
@@ -24,6 +22,41 @@ function loadUserDetails(){
             //loginResults();
             console.log("not error");
             document.getElementById("name").innerHTML = json.username
+
+        }else {
+            console.log("error");
+         //   loginResults()}
+    } 
+
+
+}
+xhr.send();
+
+}
+
+function loadPosts(){
+    var xhr = new XMLHttpRequest();
+
+    var url = "https://goga-api.herokuapp.com/api/posts?filter=%7B%22order%22%3A%22datepublication%20DESC%22%7D";
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+ 
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var json = JSON.parse(xhr.responseText);
+            console.log(json);
+            //loginResults();
+           var posts = `<div class="row">
+           <div class="col s12">
+               <div class="entry">
+                   <span class="category">Funny</span>
+                   <img src="img/post2.jpg" alt="">
+                   <h5><a href="#">Crazy really shows you have to try</a></h5>
+                   <span class="author">By <a href="#">Mario Doe</a></span>
+                   <p>Lorem ipsum dolor sit amet consectetur</p>
+               </div>
+           </div>
+       </div>`
 
         }else {
             console.log("error");
--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,28 @@
+var userId;
 window.onload = function() {
 
-    loadUserDetails();
+     this.userId = localStorage.getItem("loginDetails");
+
+    setTimeout(function(){
+        
+checkDetails();
+
+    }, 3000);
+
+
+   
     
     
+    }
+
+    function checkDetails(){
+
+        if(this.userId){
+
+            location.replace("index-mobile.html");
     
+         }else {
+            location.replace("login.html");
+    
+         }
     }
